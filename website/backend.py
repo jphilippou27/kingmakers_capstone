e71fccec0824a57f9b9fdb1ee397561d8e0bf9f9
Jennifer <jphilippou27@gmail.com>
2018-12-08 11:55:14 -0500

    industry NAN debugging

--- website/backend.py
+++ website/backend.py
@@ -173,10 +173,10 @@ def make_nodes(dataset):
     #df_nodes_P.head()
     
     #merge nodes
-    df_nodes = pd.concat([df_nodes_I, df_nodes_P])
+    #df_nodes = pd.concat([df_nodes_I, df_nodes_P])
     
     #Convert to list
-    node_list = list(df_nodes.apply(lambda row: {"name": row['firstlastp'], "group": row['party'],  "contribution_total": row['contr_amt'], "winner_ind":row['ge_winner_ind_guess']}, axis=1))
+    node_list = list(df_nodes_I.apply(lambda row: {"name": row['firstlastp'], "group": row['party'],  "contribution_total": row['contr_amt'], "winner_ind":row['ge_winner_ind_guess']}, axis=1))
     
     #export
     return(df_nodes_I)
