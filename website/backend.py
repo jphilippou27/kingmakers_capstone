471480d08e3cb5d63ec80ae447d53c69149215e2
Jennifer <jphilippou27@gmail.com>
2018-12-09 13:28:08 -0500

    adding contribution minimum 4 industry network
    to reduce complexity on network

--- website/backend.py
+++ website/backend.py
@@ -195,7 +195,7 @@ def merge_nodes_links(links_list_fv, node_list):
 
 def get_network_by_industry(firstlastp):
     cand = (str(firstlastp))
-    row = query_pg("SELECT * FROM network_industry t1 LEFT JOIN (select distinct(industry) FROM network_industry WHERE firstlastp = %s and ge_winner_ind_guess is not null) sub ON t1.industry = sub.industry WHERE (sub.industry IS NOT NULL) and (t1.contr_amt> 1)", [cand])
+    row = query_pg("SELECT * FROM network_industry t1 LEFT JOIN (select distinct(industry) FROM network_industry WHERE firstlastp = %s and ge_winner_ind_guess is not null) sub ON t1.industry = sub.industry WHERE (sub.industry IS NOT NULL) and (t1.contr_amt> 250000)", [cand])
     print(row)
     df_network_viz_fv = pd.DataFrame([i.copy() for i in row])
     links_list_fv = make_links(df_network_viz_fv)
