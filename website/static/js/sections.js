0d0eb51d53f4b2f8615c4dbe38242b28782f11e2
Jennifer <jphilippou27@gmail.com>
2018-12-09 16:11:09 -0500

    debuggind node list 1

--- website/static/js/sections.js
+++ website/static/js/sections.js
@@ -835,7 +835,7 @@ graph = d3.json('/networkdata', function(error, graph) {
 graph = d3.json('/networkNodeList', function(error, graph) {
 		//console.log(graph)
 		var optArray = [];
-		for (var i = 0; i < graph.firstlastp.length - 1; i++) {
+		for (var i = 0; i < graph.length - 1; i++) {
 			optArray.push(graph.firstlastp[i]);
 		}
 
