63c30de4b0464e37e4d9bcf9a40f36de4f039f8c
Jennifer <jennifer.p@berkeley.edu>
2018-12-10 17:46:27 -0500

    changing Left to right

--- website/backend.py
+++ website/backend.py
@@ -328,12 +328,13 @@ def make_nodes_commit(dataset):
 def get_network_committee (firstlastp):
     cand = (str(firstlastp)) 
     print("committee sql qc: ", cand) 
-    row = query_pg("SELECT contr_amt, firstlastp, ge_winner_ind_guess, t1.pacshort, party, type  \
-                        FROM committee_network t1 \
-                        LEFT JOIN (select distinct(pacshort) \
-                        FROM committee_network \
-                        WHERE firstlastp = %s and contr_amt > 1 GROUP BY pacshort)sub ON t1.pacshort = sub.pacshort \
-                        WHERE contr_amt > 100000", [cand])
+    row = query_pg("Select * FROM committee_network t1 \
+                        RIGHT JOIN (select distinct (pacshort)  \
+                                    FROM committee_network \
+                                    WHERE firstlastp = %s and contr_amt > 100000 \
+                                    GROUP BY pacshort)sub \
+                    ON t1.pacshort = sub.pacshort \
+                   WHERE contr_amt > 100000", [cand])
     row = pd.DataFrame([i.copy() for i in row])
     links_list_fv = make_links_commit(row)
     node_list = make_nodes_commit(row)
