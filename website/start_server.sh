97f336c3346281d9fddbfeeae47869cce37abeee
mattproetsch <mattproetsch@gmail.com>
2018-12-10 21:43:33 -0600

    add configurable port and HTTPS

--- website/start_server.sh
+++ website/start_server.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 PORT="${1:-8765}"
-gunicorn --bind 0.0.0.0:$PORT --daemon --user=root --group=root wsgi
+gunicorn --bind 0.0.0.0:$PORT --user=root --group=root wsgi\
+    --keyfile /etc/letsencrypt/live/kingmakerscapstone.com/privkey.pem\
+    --certfile /etc/letsencrypt/live/kingmakerscapstone.com/fullchain.pem
+
